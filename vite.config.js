9f72beee5d1435dfeb03b81d5c50d1bcafcc442d
ArpitBreathes <proton.arpitpandey@gmail.com>
2025-12-23 12:59:00 +0530

    Fix Firebase Hosting base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Personal-Advisory-Council/",
+  base: "/",
   server: {
     port: 3000,
   },
